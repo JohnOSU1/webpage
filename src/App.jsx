281644e8d293ee617a6116f05a68402139d694c8
John Polasek <polasekj@oregonstate.edu>
2025-09-01 08:31:26 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>John Polasek</h1>
+      <div className="card">
+       
+      </div>
+    </>
+  )
+}
+
+export default App
