6664c65a0f023d9fb4c9c4035135d86abaf1f070
John Polasek <polasekj@oregonstate.edu>
2025-09-03 11:26:14 -0800

    Close to done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,26 +2,18 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <div className="projects">
+    <div class="projects">
       <div>
-        <h3>2025, Website for Software Engineering</h3>
+        <h4>2025, Website for Software Engineering</h4>
         <ul>
           <li>Developed a typing practice website for coding in Node.js which included EJS for links, Bootstrap for styling, and a SQL server to store user data.  </li>
           <li> Collaborated with five peers in developing, implementing, and debugging of a webs </li>
         </ul>
       </div>
       <div>
-        <h3>2023, Alaska Dept. of Fish & Game Information Management Program Internship: </h3>
+        <h4>2024, Small Shell: </h4>
         <ul>
-          <li>Worked on department’s website which was slated to be transferred from its current platform to a new service, Umbraco. Codebase consisted primarily of CSS and JavaScript files.  Adapted online training and tools to support quick and efficient transfer.  </li>
-          <li> Updated website to include Bootstrap. Transfer included updating the menu, redesigning page contents to be more responsive, and making cleaner, sleek pages, while maintaining aspects of previous style.   </li>
-        </ul>
-      </div>
-      <div>
-        <h3>2021, Alaska Dept. of Fish & Game Information Management Program Volunteer: </h3>
-        <ul>
-          <li>Gained experience optimizing photos, coding, and uploading to the state website.   </li>
-          <li>Optimized and coded photos into web files to provide a better viewing experience. </li>
+          <li>Small shell was created in C and was able to handle input/output redirection, built-in commands, allowed for foreground and background execution, and had custom signal handlers.</li>
         </ul>
       </div>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import Code from '../components/CodeLang'
 import Experience from '../components/Experience'
+import Projects from '../components/Projects'
+import Courses from '../components/Courses'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
@@ -7,14 +9,14 @@ function Home(){
 
     return (
         <div>
-            <h2>Blurb</h2>
+            <h2>About Me</h2>
             <p>I’m graduating in 2026 and have experience building websites, games, and applications. I take pride in ensuring that everything I create reflects my full effort! Beyond development, I’ve explored the world of cybersecurity, breaking encryptions, understanding their inner workings, and diving deep into digital forensics to uncover hidden data and trace digital footprints.</p>
             <h2>Expirence</h2>
             <Experience />
             <h2>Projects</h2>
-            <p>temp</p>
+            <Projects />
             <h2>Relevent courses</h2>
-            <p> temp</p>
+            <Courses />
             <h2>Code Languages and Applications</h2>
             <Code />
         </div>
